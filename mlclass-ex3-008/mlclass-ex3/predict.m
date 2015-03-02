7914e72aaa39057f9bdd1e1827489ceac41ebb12
linus-young <ylm19921123@msn.cn>
2015-03-02 15:32:34 +0800

    solution for ex3

--- mlclass-ex3-008/mlclass-ex3/predict.m
+++ mlclass-ex3-008/mlclass-ex3/predict.m
@@ -7,12 +7,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -21,12 +21,17 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
 
+% The hidden layer
+a2 = sigmoid(Theta1 * X');
 
+% Add ones to the a2 matrix
+a2 = [ones(1, size(X,1)); a2];
 
-
-
-
+% From layer 2(hidden layer) to output layer
+[max_values, p] = max(sigmoid(a2' * Theta2'), [], 2);
 
 
 % =========================================================================
